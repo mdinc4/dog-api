5d5cba5134d5fa8a0bfaa4616d0727373b21079c
mdinc4 <dincmhmd@gmail.com>
2025-04-08 10:38:53 +0300

    Added error image

--- script.js
+++ script.js
@@ -18,11 +18,12 @@ const fetchDogImage=  () => {
         imageContainer.innerHTML= `<img src="${data.message}" class="dogImage" alt="Dog Image" />`;
     }).catch((error) => {
         console.error("There was a problem with the fetch operation:", error);
-        imageContainer.innerHTML= `<p>Error fetching dog image: ${error.message}</p>`;
+        imageContainer.innerHTML= `<p>Error fetching dog image: ${error.message}</p>
+        <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_1222.jpg" class="dogImage" alt="Dog Image" />`;
     })
 }
         
-
+//errorUrl="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_1222.jpg"; if there is no picture beacuse of API error, we can use this URL to test the error handling
 
 
 
